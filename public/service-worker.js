619fbff0583ae80f89a3bbbec79cfa8ffb2aa483
Kylie S <kyliemegan24@gmail.com>
2020-11-16 16:02:55 -0600

    trying to debug heroku error

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,6 +13,8 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+
+
 // install
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
